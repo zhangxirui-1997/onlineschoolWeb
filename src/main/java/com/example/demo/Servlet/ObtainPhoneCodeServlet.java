c6a3369edcb396d677b27a95c54b85c72af8d980
cdz <2242877026@qq.com>
2020-05-05 10:45:39 +0800

    验证码

--- src/main/java/com/example/demo/Servlet/ObtainPhoneCodeServlet.java
+++ src/main/java/com/example/demo/Servlet/ObtainPhoneCodeServlet.java
@@ -11,10 +11,7 @@ import com.aliyuncs.profile.DefaultProfile;
 import com.example.demo.Tools.destPhoneUtil;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Timer;
@@ -36,9 +33,12 @@ public class ObtainPhoneCodeServlet extends HttpServlet {
         //获取注册手机号
         String phonenumberstring = request.getParameter("user_phonenumber");
         //请求验证码  发送到手机
-        String phoneCode = destPhoneUtil.phone(phonenumberstring);
-        //String phoneCode = "123456";
+//        String phoneCode = destPhoneUtil.phone(phonenumberstring);
+        String phoneCode = "123456";
 
+        Cookie cookie = new Cookie(phonenumberstring, phoneCode);
+        cookie.setMaxAge(4*60);
+        response.addCookie(cookie);
         //返回给前端数据
         PrintWriter out = response.getWriter();
         out.print(phoneCode);
