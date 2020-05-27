31b954165b67b065e6a44ed9cfe6edf3e08aa736
zhangjun <3396417346@qq.com>
2020-05-27 12:40:00 +0800

    今天修改打包

--- src/test/java/com/example/demo/OnlineschoolWebApplicationTests.java
+++ src/test/java/com/example/demo/OnlineschoolWebApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineschoolWebApplicationTests {
+public class OnlineschoolWebApplicationTests {
 
     @Test
-    void contextLoads() {
+   public void contextLoads() {
     }
 
 }
